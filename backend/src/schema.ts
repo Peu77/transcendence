c439df880b00e7b84a1e3ee202ec483f22421beb
Emil Ebert <emilebert@icloud.com>
2025-09-14 13:49:31 +0200

    add 2FA table and update timestamp field naming in schema

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -5,6 +5,14 @@ create table if not exists users (
     id text primary key,
     email text unique not null,
     password text not null,
-    created_at timestamp current_timestamp not null
+    createdAt timestamp current_timestamp not null
+);
+
+create table if not exists users_2fa (
+    id text primary key,
+    userId text not null references users(id) on delete cascade,
+    secret text not null,
+    createdAt timestamp current_timestamp not null,
+    expiredAt timestamp not null    
 );
 `
