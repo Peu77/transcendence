5832518dc8b0e01f06742fb4da7257607704ab25
Emil Ebert <emilebert@icloud.com>
2025-09-27 16:56:15 +0200

    add pixel corners

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,3 +1,4 @@
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: [
     './index.html',
@@ -6,6 +7,51 @@ module.exports = {
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+      plugin(function({ addUtilities }) {
+          addUtilities({
+              '.clip-pixel-corners': {
+                  'clip-path': `polygon(
+            0px calc(100% - 20px),
+            4px calc(100% - 20px),
+            4px calc(100% - 12px),
+            8px calc(100% - 12px),
+            8px calc(100% - 8px),
+            12px calc(100% - 8px),
+            12px calc(100% - 4px),
+            20px calc(100% - 4px),
+            20px 100%,
+            calc(100% - 20px) 100%,
+            calc(100% - 20px) calc(100% - 4px),
+            calc(100% - 12px) calc(100% - 4px),
+            calc(100% - 12px) calc(100% - 8px),
+            calc(100% - 8px) calc(100% - 8px),
+            calc(100% - 8px) calc(100% - 12px),
+            calc(100% - 4px) calc(100% - 12px),
+            calc(100% - 4px) calc(100% - 20px),
+            100% calc(100% - 20px),
+            100% 20px,
+            calc(100% - 4px) 20px,
+            calc(100% - 4px) 12px,
+            calc(100% - 8px) 12px,
+            calc(100% - 8px) 8px,
+            calc(100% - 12px) 8px,
+            calc(100% - 12px) 4px,
+            calc(100% - 20px) 4px,
+            calc(100% - 20px) 0px,
+            20px 0px,
+            20px 4px,
+            12px 4px,
+            12px 8px,
+            8px 8px,
+            8px 12px,
+            4px 12px,
+            4px 20px,
+            0px 20px
+          )`
+              }
+          })
+      })
+  ]
 };
 
