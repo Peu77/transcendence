99f01250874be9d0cc43d0911f86eeb928b2baeb
Emil Ebert <emilebert@icloud.com>
2025-09-27 23:51:33 +0200

    remove unused

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -55,46 +55,6 @@ module.exports = {
   plugins: [
     plugin(function ({ addUtilities }) {
       addUtilities({
-        ".clip-pixel-corners": {
-          "clip-path": `polygon(
-            0px calc(100% - 20px),
-            4px calc(100% - 20px),
-            4px calc(100% - 12px),
-            8px calc(100% - 12px),
-            8px calc(100% - 8px),
-            12px calc(100% - 8px),
-            12px calc(100% - 4px),
-            20px calc(100% - 4px),
-            20px 100%,
-            calc(100% - 20px) 100%,
-            calc(100% - 20px) calc(100% - 4px),
-            calc(100% - 12px) calc(100% - 4px),
-            calc(100% - 12px) calc(100% - 8px),
-            calc(100% - 8px) calc(100% - 8px),
-            calc(100% - 8px) calc(100% - 12px),
-            calc(100% - 4px) calc(100% - 12px),
-            calc(100% - 4px) calc(100% - 20px),
-            100% calc(100% - 20px),
-            100% 20px,
-            calc(100% - 4px) 20px,
-            calc(100% - 4px) 12px,
-            calc(100% - 8px) 12px,
-            calc(100% - 8px) 8px,
-            calc(100% - 12px) 8px,
-            calc(100% - 12px) 4px,
-            calc(100% - 20px) 4px,
-            calc(100% - 20px) 0px,
-            20px 0px,
-            20px 4px,
-            12px 4px,
-            12px 8px,
-            8px 8px,
-            8px 12px,
-            4px 12px,
-            4px 20px,
-            0px 20px
-          )`,
-        },
         ".clip-pixel-corners-btn": {
           "clip-path": `polygon(
             0px calc(100% - 8px),
