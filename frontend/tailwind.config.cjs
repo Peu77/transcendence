b53a8df5d554166c946440a5293495862ad27330
Emil Ebert <emilebert@icloud.com>
2025-09-28 13:27:56 +0200

    fix: adjust scale-in keyframe for improved animation effect

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -52,7 +52,7 @@ module.exports = {
       },
       keyframes: {
         "scale-in": {
-          "0%": { opacity: "0", transform: "scale(0.96)" },
+          "0%": { opacity: "0", transform: "scale(0.80)" },
           "100%": { opacity: "1", transform: "scale(1)" },
         },
         "toast-in-right": {
