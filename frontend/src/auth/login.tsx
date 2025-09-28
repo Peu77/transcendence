8001106f38929a841a9b481c5eee186903fd36cb
Emil Ebert <emilebert@icloud.com>
2025-09-28 15:23:52 +0200

    remove deprecated email function in zod validation

--- frontend/src/auth/login.tsx
+++ frontend/src/auth/login.tsx
@@ -19,7 +19,7 @@ import {
 } from "../components/Form";
 
 const loginSchema = z.object({
-  email: z.string().email("Please enter a valid email"),
+  email: z.email("Please enter a valid email"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
