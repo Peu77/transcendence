ee859ca98e548398bbf122af34d7799f9e4efea5
Matthias-Strauss <matthias.strauss@proton.me>
2025-09-29 16:27:11 +0200

    smart contract: changed vars from memory to calldata for efficiency

--- blockchain/contract.sol
+++ blockchain/contract.sol
@@ -4,9 +4,19 @@ pragma solidity ^0.8.0;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PongScores is Ownable {
-   constructor(address initialOwner) Ownable(initialOwner) {}
+    int public highScore;
+    string public uuid;
+    string public nickName;
 
-    function seetHighScore() public onlyOwner{}
+    constructor(address initialOwner) Ownable(initialOwner) {}
 
-    function getHighScore() public {}
+    function setHighScore(int _score, string calldata _uuid, string calldata _nickName) public onlyOwner {
+        highScore = _score;
+        uuid = _uuid;
+        nickName = _nickName;
+    }
+
+    function getHighScore() external view returns (int, string calldata, string calldata) {
+        return (highScore, uuid, nickName);
+    }
 }
