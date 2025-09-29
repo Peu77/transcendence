589cdbb3bc4690cf5a9ceb1044e570e9b4acbba1
Matthias-Strauss <matthias.strauss@proton.me>
2025-09-29 16:41:03 +0200

    smart contract: further data packing, switching to uint for non negative values.

--- blockchain/contract.sol
+++ blockchain/contract.sol
@@ -4,19 +4,24 @@ pragma solidity ^0.8.0;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PongScores is Ownable {
-    int public highScore;
-    string public uuid;
-    string public nickName;
+
+    struct ScoreData {
+    uint128 highScore;
+    uint128 uuid;
+    bytes32 nickName;
+    }
+
+    ScoreData public scoreData;
 
     constructor(address initialOwner) Ownable(initialOwner) {}
 
-    function setHighScore(int _score, string calldata _uuid, string calldata _nickName) public onlyOwner {
-        highScore = _score;
-        uuid = _uuid;
-        nickName = _nickName;
+    function setHighScore(uint128 _score, uint128 _uuid, bytes32 _nickName) public onlyOwner {
+        scoreData.highScore = _score;
+        scoreData.uuid = _uuid;
+        scoreData.nickName = _nickName;
     }
 
-    function getHighScore() external view returns (int, string calldata, string calldata) {
+    function getHighScore() external view returns (uint128, uint128, bytes32) {
         return (highScore, uuid, nickName);
     }
 }
