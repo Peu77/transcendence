2bcbf90389abe18c4786cf9b43fba67b06850e9c
Emil <60301119+Peu77@users.noreply.github.com>
2025-10-04 16:14:14 +0200

    Revert "Revert "Auth middleware""

---
+++ frontend/src/api/user.ts
@@ -0,0 +1,13 @@
+import { axios } from "../query/client";
+
+export type User = {
+  id: string;
+  email: string;
+  twoFaEnabled: boolean;
+}
+
+export async function getUser(){
+  const res = await axios.get<User>("/users/me")
+  return res.data;
+}
+
