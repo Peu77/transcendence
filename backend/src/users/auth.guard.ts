a472061ffeb81c69d39a81965d386864ebeb8fc7
Emil Ebert <emilebert@icloud.com>
2025-10-04 15:42:24 +0200

    format backend with prettier

--- backend/src/users/auth.guard.ts
+++ backend/src/users/auth.guard.ts
@@ -1,19 +1,19 @@
-import {app} from "../app";
-import jwt from 'jsonwebtoken';
+import { app } from "../app";
+import jwt from "jsonwebtoken";
 
-export function registerAuthGuard(){
-    app.addHook('onRequest', async (request, reply) => {
-        if(request.url.startsWith('/auth/')) return;
+export function registerAuthGuard() {
+  app.addHook("onRequest", async (request, reply) => {
+    if (request.url.startsWith("/auth/")) return;
 
-        const token = request.cookies.token;
-        if(!token) return reply.code(401).send({error: 'Unauthorized'});
+    const token = request.cookies.token;
+    if (!token) return reply.code(401).send({ error: "Unauthorized" });
 
-        jwt.verify(token, process.env.JWT_SECRET || "", (err: any, decoded) => {
-            if(err) return reply.code(401).send({error: 'Unauthorized'});
-            if(!decoded || typeof decoded === 'string' || !decoded.userId)
-                return reply.code(401).send({error: 'Unauthorized'});
+    jwt.verify(token, process.env.JWT_SECRET || "", (err: any, decoded) => {
+      if (err) return reply.code(401).send({ error: "Unauthorized" });
+      if (!decoded || typeof decoded === "string" || !decoded.userId)
+        return reply.code(401).send({ error: "Unauthorized" });
 
-            request.userId = decoded.userId;
-        })
+      request.userId = decoded.userId;
     });
+  });
 }
