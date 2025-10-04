6ba4a93e2f0249e07c279cd7df7839ab764b57fe
Emil Ebert <emilebert@icloud.com>
2025-10-04 15:10:07 +0200

    changes

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,5 +1,4 @@
 import { h, Fragment } from "refreshjs";
-import { RetroGameHeader } from "./layout/RetroGameHeader";
 import { RetroNavigation } from "./layout/RetroNavigation";
 import { TableTennis3D } from "./layout/TableTennis3D";
 import { ThreeDMesh } from "./layout/3DMesh";
@@ -7,7 +6,6 @@ import { ThreeDMesh } from "./layout/3DMesh";
 export default function App() {
   return (
     <Fragment>
-      <RetroGameHeader />
       <RetroNavigation />
       <TableTennis3D onTableClick={() => {}} />
       <ThreeDMesh />
