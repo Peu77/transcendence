71f6fe45814e427087554dc1e2cbd42b380803ae
Emil Ebert <emilebert@icloud.com>
2025-10-15 15:55:11 +0200

    current user will now be checked for the access of wrapper component and it's children

--- frontend/src/app/layout/Layout.tsx
+++ frontend/src/app/layout/Layout.tsx
@@ -1,8 +1,41 @@
-import { h } from "refreshjs";
+import { h, navigate} from "refreshjs";
 import { RetroNavigation } from "./RetroNavigation";
 import { ThreeDMesh } from "./3DMesh";
+import { getUser } from "@/api/user";
+import { User } from "@/api/user";
+
+
+let _currentUser: User | null | undefined = undefined;
+
+export let currentUser: User | null = null;
+
+export async function initAuth(): Promise<User | null> {
+  try {
+    const user = await getUser();
+    if (user) {
+      currentUser = user;
+      return user;
+    } else {
+      return null;
+    }
+  } catch {
+    return null;
+  }
+}
+
 
 export default function Layout(props: any) {
+  if (typeof window !== "undefined" && _currentUser === undefined) {
+    _currentUser = null;
+    (async () => {
+      const user = await initAuth();
+      _currentUser = user;
+      if (!user) {
+        navigate("/login");
+      }
+    })();
+  }
+
   return (
     <div>
       <RetroNavigation />
