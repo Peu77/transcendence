347ff63cb1bf2573ecc3e652f0cdbe9da7a263f4
unicorn453 <kira.ruseva@gmail.com>
2025-10-20 11:03:44 +0200

    simple table and movement of ball added for testing

--- frontend/src/app/layout/Game.tsx
+++ frontend/src/app/layout/Game.tsx
@@ -1,10 +1,17 @@
-import { h } from "refreshjs";
+import { h, useEffect, useRef } from "refreshjs";
 import { GameModeSelector } from "./GameModeSelector";
+import { singlePlayerGame } from "../../game/single_player";
 
 export default function Game() {
-  return (
-    <div className="mt-10">
-      <GameModeSelector />
-    </div>
-  );
+
+  const containerId = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    if (containerId.current) {
+      singlePlayerGame(containerId.current);
+    }
+  }
+  , []);
+
+  return <div ref={containerId} style={{ width: "100%", height: "100vh" }} />;
 }
