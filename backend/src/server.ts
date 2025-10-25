09801a46586d5072b2cc09af2004d98d5c276efc
Komu211 <75761278+Komu211@users.noreply.github.com>
2025-10-25 13:28:24 +0200

    Add very simple example test, fix circular dependency

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,5 @@
 import { buildServer } from "./app";
-import { config } from "dotenv";
-
-config();
-
-export function getEnv(key: string): string {
-  const value = process.env[key];
-  if (!value) {
-    throw new Error(`Missing env var ${key}`);
-  }
-  return value;
-}
+import { getEnv } from "./utils/env";
 
 const PORT = Number(process.env.PORT) || 4000;
 const HOST = process.env.HOST || "0.0.0.0";
