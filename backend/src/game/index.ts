23feff2b561339a4f4b66db9d85407041df90ffc
Emil Ebert <emilebert@icloud.com>
2025-12-12 21:30:38 +0100

    delete old backend

--- backend/src/game/index.ts
+++
@@ -1,5 +0,0 @@
-export { GameRoomManager } from './roomManager';
-export { GameEngine } from './engine';
-export { registerGameRoutes } from './routes';
-export { registerGameWebSocket } from './websocket';
-export * from './types';
