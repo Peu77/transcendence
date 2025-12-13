64ec14b6cf6bf4a3fab8c77fa2f3a486912080fa
Emil Ebert <emilebert@icloud.com>
2025-12-13 15:13:03 +0100

    add app page and fix navigation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000',
     credentials: true,
   });
   app.useGlobalPipes(
