a9381f6c8bed56c2250d82412260ee990c0e7eac
Emil Ebert <emilebert@icloud.com>
2025-12-20 15:20:20 +0100

    add profileImage component

---
+++ frontend/src/components/app/profileImage.tsx
@@ -0,0 +1,14 @@
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar.tsx";
+import {env} from "@/env.ts";
+import DefaultProfileImage from "@/assets/defaultProfilePicture.webp";
+
+export const ProfileImage = (props: { profilePictureId: string | null | undefined }) => {
+    const src = props.profilePictureId ? `${env.VITE_BACKEND_URL}/users/profilePicture/${props.profilePictureId}` : DefaultProfileImage;
+
+    return (
+        <Avatar>
+            <AvatarImage src={src}/>
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+    )
+}
