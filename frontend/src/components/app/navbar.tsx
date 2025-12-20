3923ecea340dfeb6d85fc42ac67a4cbc16dc0936
Emil Ebert <emilebert@icloud.com>
2025-12-20 15:20:46 +0100

    improve navbar

--- frontend/src/components/app/navbar.tsx
+++ frontend/src/components/app/navbar.tsx
@@ -1,11 +1,16 @@
 import {useState} from "react";
 import {Button} from "@/components/ui/button.tsx";
-import {MoonIcon, SunIcon} from "lucide-react";
+import {MoonIcon, SunIcon, UsersIcon} from "lucide-react";
+import {ProfileImage} from "@/components/app/profileImage.tsx";
+import {userStore} from "@/store/userStore.ts";
+import {useStore} from "@tanstack/react-form";
 
 export const Navbar = () => {
     const [isDark, setIsDark] = useState(() =>
         document.documentElement.classList.contains("dark"),
     );
+    const profileImageId = useStore(userStore, (state) => state?.profilePictureId);
+    const username = useStore(userStore, (state) => state?.username);
 
     const toggleDarkMode = () => {
         document.documentElement.classList.toggle("dark");
@@ -13,14 +18,29 @@ export const Navbar = () => {
     };
 
     return (
-        <nav className="w-full h-16 bg-card flex items-center px-4 shadow-sm">
-            <div className="text-xl font-bold ">
+        <nav className="w-full bg-card flex items-stretch px-2 h-16 justify-between shadow-sm">
+            <div className="text-xl font-bold flex items-center">
                 Transcendence
             </div>
 
-            <Button variant="ghost" onClick={toggleDarkMode} className="ml-auto">
-                {isDark ? <MoonIcon/> : <SunIcon/>}
-            </Button>
+            <div className="flex items-stretch mt-1 mb-3">
+                <div className="flex items-stretch gap-4">
+                    <div className="flex items-center">
+                        <Button variant="ghost" onClick={toggleDarkMode}>
+                            {isDark ? <MoonIcon/> : <SunIcon/>}
+                        </Button>
+                        <Button variant="ghost">
+                            <UsersIcon/>
+                        </Button>
+                    </div>
+
+                    <div className="flex gap-10 px-1 items-center bg-secondary clip-pixel-corners-btn cursor-pointer select-none hover:translate-y-1 active:translate-y-2 transition-all">
+                        <p className="ml-2 font-bold text-l">{username}</p>
+                        <ProfileImage profilePictureId={profileImageId}/>
+                    </div>
+                </div>
+            </div>
+
         </nav>
     )
 }
