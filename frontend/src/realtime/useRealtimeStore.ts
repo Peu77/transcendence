ec488226723e980b85ed505e3bfdaf53f2703967
Emil Ebert <emilebert@icloud.com>
2025-12-21 15:29:42 +0100

    add realtime store and websocket connection handling with RealtimeMount integration

---
+++ frontend/src/realtime/useRealtimeStore.ts
@@ -0,0 +1,6 @@
+import { useStore } from "@tanstack/react-store";
+import { realtimeStore, type RealtimeState } from "./store";
+
+export function useLiveSocket() {
+  return useStore(realtimeStore, (s: RealtimeState) => s.socket);
+}
