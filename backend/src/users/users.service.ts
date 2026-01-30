83d8c99d08c5153a083b09568e20f981a37231df
Emil Ebert <emilebert@icloud.com>
2026-01-30 22:14:07 +0100

    add 2FA support in backend and integrate qrcode.react package in frontend

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -25,6 +25,14 @@ export class UsersService {
     await this.usersRepo.update({ id: userId }, { profilePictureId });
   }
 
+  async updateTwoFaSecret(userId: string, twoFaSecret: string | null): Promise<void> {
+    await this.usersRepo.update({ id: userId }, { twoFaSecret });
+  }
+
+  async enableTwoFa(userId: string): Promise<void> {
+    await this.usersRepo.update({ id: userId }, { twoFaEnabled: true });
+  }
+
   async getUserInfo(userId: string): Promise<UserInfo>{
     const user = await this.usersRepo.findOneOrFail({ where: { id: userId } });
     return {
