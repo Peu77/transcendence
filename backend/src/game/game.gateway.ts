4542a480ba691252b7db52336b48f598bfbe7748
Jonas Götz <jonas@42heilbronn.de>
2026-06-13 12:22:54 +0200

    remove old references of port 443

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -8,7 +8,7 @@ import type { Server } from 'socket.io'
 @WebSocketGateway({
   namespace: '/game',
   cors: {
-    origin: process.env.CORS_ORIGIN ?? 'https://localhost',
+    origin: process.env.CORS_ORIGIN ?? 'https://localhost:8443',
     credentials: true,
   },
 })
